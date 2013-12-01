348851f82b7f83a3347480796057b12f6a6b432d
Yaron <justame@gmail.com>
2013-12-01 16:06:25 +0200

    added banner interval

--- app/models/spree/banner_configuration.rb
+++ app/models/spree/banner_configuration.rb
@@ -5,5 +5,6 @@ module Spree
     preference :banner_url, :string, default: '/spree/banners/:id/:style/:basename.:extension'
     preference :banner_styles, :string, default: "{\"mini\":\"48x48>\",\"small\":\"100x100>\",\"large\":\"800x200#\"}"
     preference :banner_default_style, :string, default: 'small'
+    preference :banner_home_interval, :integer, default: 4
   end
 end
