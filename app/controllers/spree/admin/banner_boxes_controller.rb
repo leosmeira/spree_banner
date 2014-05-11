766f9cebdf56d09bc892a47ab20316fb6107aa91
Gady Schujovitzky <gaaady@gmail.com>
2014-05-11 17:49:53 +0300

    order by position

--- app/controllers/spree/admin/banner_boxes_controller.rb
+++ app/controllers/spree/admin/banner_boxes_controller.rb
@@ -1,15 +1,15 @@
 module Spree
   module Admin
     class BannerBoxesController < ResourceController
-      
+
       def index
         respond_with(@collection)
       end
-      
+
       def show
         redirect_to( :action => :edit )
       end
-      
+
       def update
         @banner_box.enhance_settings
         super
@@ -26,22 +26,22 @@ module Spree
 
         respond_with(@new) { |format| format.html { redirect_to edit_admin_banner_box_url(@new) } }
       end
-      
+
       protected
       def find_resource
         Spree::BannerBox.find(params[:id])
       end
-      
+
       def location_after_save
          edit_admin_banner_box_url(@banner_box)
       end
-      
+
       def collection
         return @collection if @collection.present?
         params[:q] ||= {}
-        params[:q][:s] ||= "category, position asc"
-        
+
         @search = super.ransack(params[:q])
+        @search.sorts = 'position'
         @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
       end
     end
