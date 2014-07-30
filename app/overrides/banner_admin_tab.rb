0b17cf620d9a356e81ed68f9f202d15d974a30cf
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2014-07-30 16:50:28 -0300

    Update icon tab on admin

--- app/overrides/banner_admin_tab.rb
+++ app/overrides/banner_admin_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                       :name => "banner_box_admin_tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<%= tab(:banner_boxes, :icon => 'icon-bookmark') %>")
+                      :text => "<%= tab(:banner_boxes, :icon => 'picture-o') %>")
 
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                       :name => "add_banner_box_settings",
